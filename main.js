5bfa6006550bce957a3d38ab27298280b7076c68
Isaiah St. John <hello@isaiah.st>
2020-02-01 20:45:00 -0500

    refine the concept

--- main.js
+++ main.js
@@ -2,27 +2,31 @@ function escape(s){
   return s.replace(/[&<>]/g, c => "&#" + c.charCodeAt(0) + ";");
 }
 
-
-function Value(initialValue, validator){
-  this._value = initialValue;
+function Value({updater}={}){
+  this.update = updater;
+  this._value = undefined;
   this._listeners = [];
-  this._validator = validator;
-  this.FAIL = 1;
+  this._alters = [];
 }
 
-Value.prototype.get = function(){ return this._value; }
+Value.prototype.value = function(){ return this._value; }
 Value.prototype.reg = function(listener){ 
   this._listeners.push(listener);
 }
 Value.prototype.set = function(value){
-  if(this._validator && !this._validator(value, this._value)){
-    return this.FAIL;
-  }
+  if(this.update) return; // A value can either be set() or update()
   const oldValue = this._value;
   this._value = value;
   this._listeners.forEach(listener => listener(value, oldValue));
+  this._alters.forEach(altered => altered.update());
 }
-let titleEl = document.getElementById("output");
+Value.prototype.alters = function(...values){
+  this._alters.push(...values);
+}
+
+let wordRegEx = /\b[\w'-]+\b/g;
+
+let title = document.getElementById("output");
 let inputBox = document.getElementById("input");
 let charCount = document.getElementById("char-count");
 let wordCharCount = document.getElementById("word-char-count");
@@ -31,77 +35,73 @@ let aveLen = document.getElementById("ave-len");
 let punctuationList = document.getElementById("punctuation-list");
 let wordList = document.getElementById("word-list");
 
-let title = new Value("");
-let wordCharCountVal = new Value(0);
-let wordCountVal = new Value(0);
+let titleVal = new Value();
+inputBox.addEventListener('keyup', 
+  () => titleVal.set(escape(inputBox.value)));
 
-inputBox.addEventListener('keyup', () => title.set(inputBox.value));
-/*
- * [title]
- * charCount, punctuationList, wordList
- *  [wordCharCount]
- *    aveLen
- *  [wordCount]
- *    aveLen
-*/
-title.reg(displayTitle);
-title.reg(displayCharCount);
-title.reg(displayPunctuationList);
-title.reg(displayWordList);
+let charCountVal = new Value({
+  updater: function (){
+    this.set(titleVal.value().length);
+  }});
 
-title.reg(updateWordCharCount);
-title.reg(updateWordCount);
+let wordCharCountVal = new Value({
+  updater: function (){
+    let wcc = 0;
+    let matches = titleVal.value().match(wordRegEx);
+    if(matches){
+      wcc = matches.reduce((acc, cur) => acc + cur.length, 0);
+    }
+    this.set(wcc);
+  }});
 
-wordCharCountVal.reg(displayWordCharCount);
-wordCharCountVal.reg(displayAveLen);
+let wordCountVal = new Value({
+  updater: function () {
+    let words = titleVal.value().match(wordRegEx);
+    let wc = words ? words.length : 0;
+    this.set(wc);
+  }});
 
-wordCountVal.reg(displayWordCount);
-wordCountVal.reg(displayAveLen);
+let aveLenVal = new Value({
+  updater: function () {
+    let avelen = wordCharCountVal.value() / wordCountVal.value();
+    this.set(avelen.toFixed(2));
+  }});
 
-let wordRegEx = /\b[\w'-]+\b/g;
+let punctuationListVal = new Value({
+  updater: function (){
+    let list = [... new Set(titleVal.value().match(/[^\w\s]/g))]
+      .sort()
+      .join('');
+    this.set(escape(list));
+  }});
 
-function updateWordCharCount(){
-  let wcc = 0;
-  let matches = title.get().match(wordRegEx);
-  if(matches){
-    wcc = matches.reduce((acc, cur) => acc + cur.length, 0);
-  }
-  wordCharCountVal.set(wcc);
-}
-function updateWordCount(){
-  let words = title.get().match(wordRegEx);
-  let wc = words ? words.length : 0;
-  wordCountVal.set(wc);
-  wordCount.innerHTML = wc;
-}
-function displayTitle(){
-  titleEl.innerText = title.get();
-}
-function displayCharCount(){
-  charCount.innerText = title.get().length;
-}
-function displayPunctuationList(){
-  let list = [... new Set(title.get().match(/[^\w\s]/g))]
-    .sort()
-    .join('');
-  punctuationList.innerHTML = escape(list);
-}
-function displayWordList(){
-  let list = [... new Set(title.get().toLowerCase().match(wordRegEx))]
-    .sort()
-    .map(word => escape(word))
-    .join(" ");
-  wordList.innerHTML = escape(list);
-}
-function displayWordCharCount(){
-  console.log(wordCharCountVal.get());
-  wordCharCount.innerText = wordCharCountVal.get();
+let wordListVal = new Value({
+  updater: function () {
+    let list = [... new Set(
+      titleVal.value().toLowerCase().match(wordRegEx)
+    )]
+      .sort()
+      .join(" ");
+    this.set(escape(list));
+  }});
 
-}
-function displayWordCount(){
-  wordCount.innerText = wordCountVal.get();
-}
-function displayAveLen(){
-  let avelen = wordCharCountVal.get() / wordCountVal.get();
-  aveLen.innerHTML = avelen.toFixed(2);
-}
+[[titleVal, title],
+  [charCountVal, charCount],
+  [wordCharCountVal, wordCharCount],
+  [wordCountVal, wordCount],
+  [aveLenVal, aveLen],
+  [punctuationListVal, punctuationList],
+  [wordListVal, wordList]
+].forEach(([val, target]) => 
+  val.reg(() => target.innerText = val.value()));
+
+titleVal.alters(charCountVal,
+  wordCharCountVal,
+  wordCountVal,
+  punctuationListVal,
+  wordListVal);
+
+wordCharCountVal.alters(aveLenVal);
+
+wordCountVal.alters(aveLenVal);
+ 
